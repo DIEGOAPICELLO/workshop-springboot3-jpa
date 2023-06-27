58f69900b99668301731be15b450f967b75cc7dd
Diego Apicello <manoburn@gmail.com>
2023-06-27 20:38:26 -0300

    User Update

--- courseSpring/src/main/java/com/mburn/courseSpring/resources/UserResource.java
+++ courseSpring/src/main/java/com/mburn/courseSpring/resources/UserResource.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -36,8 +35,16 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 
-
-
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+        obj = service.update(id, obj);
+        return ResponseEntity.ok().body(obj);
+    }
 
 }
