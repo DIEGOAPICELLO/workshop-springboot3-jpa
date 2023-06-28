9c4b0ebd03986963f7bd45dae774086871486b9f
Diego Apicello <manoburn@gmail.com>
2023-06-28 07:43:23 -0300

    Exception handling - delete

--- courseSpring/src/main/java/com/mburn/courseSpring/resources/exceptions/ResourceExceptionHandler.java
+++ courseSpring/src/main/java/com/mburn/courseSpring/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.mburn.courseSpring.resources.exceptions;
 
+import com.mburn.courseSpring.services.exceptions.DatabaseException;
 import com.mburn.courseSpring.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.apache.catalina.filters.ExpiresFilter;
@@ -17,6 +18,15 @@ public class ResourceExceptionHandler {
 
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
